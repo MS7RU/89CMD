54d73dc240be021da71c4d1a757a008bd3f1e93b
MS7RU <88928636+MS7RU@users.noreply.github.com>
2022-02-02 16:16:31 +0530

    Update cmd.js

--- cmd.js
+++ cmd.js
@@ -1,6 +1,7 @@
 function output() {
     if (document.getElementById ("reply").value == "./clr") {
         document.getElementById ("Output").innerHTML = "&nbsp;89CMD [Version  " + vertion + "] <br>"
+        document.getElementById ("reply").value = ""
     } else if (document.getElementById ("reply").value == "./ver"){
        var1 = "89CMD [Version  " + vertion + "]"
     }else {
