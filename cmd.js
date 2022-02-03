d84e3a2daf529d78bc51f20b17e584feba74402d
MS7RU <88928636+MS7RU@users.noreply.github.com>
2022-02-03 12:00:48 +0530

    Add files via upload

---
+++ cmd.js
@@ -0,0 +1,19 @@
+function output() {
+
+    if (text == "./clr") {
+        document.getElementById ("Output").innerHTML = "&nbsp;89CMD [Version  " + vertion + "] <br>"
+        document.getElementById ("reply").value = ""
+    } else if (text == "./ver"){
+       var1 = "89CMD [Version  " + vertion + "]"
+    } else if (text.split(" ")[0] == "./user"){
+        var1 = "Username is set from'" + user +"' to '" + text.split(" ")[1] + "'."
+        user = text.split(" ")[1]
+    } else {
+        var1 = "'" +document.getElementById ("reply").value + "' is not recognized as an command."
+    }
+
+
+    end(var1)
+}
+
+
